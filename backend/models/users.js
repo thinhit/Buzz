d8773c322493df0b5b4f7910173c3d2719e2d38b
Thinh <thinhit@yahoo.com>
2014-10-19 17:13:53 +0700

    Update user router API

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,14 +1,50 @@
-(function (){
+(function () {
     var User = require('../database/mongodb').Users,
-        jwt = require('jwt-simple');
-    function _getUserInfo(userId, callback){
-        User.findById(userId, function (err, resp){
+        jwt = require('jwt-simple'),
+        async = require('async'),
+        crypto = require('crypto');
+
+    function _getUserInfo(userId, callback) {
+        User.findById(userId, function (err, resp) {
             callback(err, resp);
         })
     }
 
-    function updateUserInfo(userId, update,callback){
+    function _updateUserInfo(userId, data, callback) {
+        User.findByIdAndUpdate(userId, data, function (err, resp) {
+            callback(err, resp);
+        })
+    }
 
+    function _registerUser(userInfo, callback) {
+        User.create(userInfo, function (err, resp) {
+            callback(err, resp);
+        })
+    }
+
+    function _login(username, password, callback) {
+        async.waterfall([
+            function (callback) {
+                User.findOne({username: username}, function (err, data) {
+                    if (err) return callback(err, null);
+                    if (data.length > 0) {
+                        callback(null, data);
+                    } else {
+                        callback('ERROR.USER', null);
+                    }
+                })
+            },
+            function (user){
+                var userPass = crypto.createHash('md5').update(password).digest('hex');
+                if(user.password == userPass){
+                    callback(null, user);
+                }else {
+                    callback('ERROR.PASSWORD', null)
+                }
+            }
+        ], function (err, resp) {
+            callback(err, resp);
+        })
     }
 
 })();
