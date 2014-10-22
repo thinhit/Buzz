b749434937cbba45852d38c9d2ea3fa2aafd8e49
thinhjs <thinhit@yahoo.com>
2014-10-23 02:21:29 +0700

    update function login and register

--- frontend/app/scripts/controllers/ProjectCtrl.js
+++ frontend/app/scripts/controllers/ProjectCtrl.js
@@ -3,8 +3,17 @@
 angular.module('Buzz')
     .controller('ProjectCtrl', ['$scope', '$state', '$http', '$auth',
         function ($scope, $state, $http, $auth) {
-            if(!$auth.getUser()){
+
+            if (!$auth.getUser()) {
                 $state.go('login');
             }
+
+
+            $scope.selectProject = function (project){
+
+                window.localStorage.setItem('currentProject', project);
+                $state.go('buzz.home');
+            }
+
 
         }]);
