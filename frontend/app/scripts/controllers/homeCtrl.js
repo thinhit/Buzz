d1838049099c1f3d813cc99259b8f792a6df0c2d
thinhjs <thinhit@yahoo.com>
2014-10-23 23:40:35 +0700

    Update list rooms

--- frontend/app/scripts/controllers/homeCtrl.js
+++ frontend/app/scripts/controllers/homeCtrl.js
@@ -18,36 +18,54 @@ angular.module('Buzz')
                 {
                     name: chanel.name,
                     creator: $auth.getUser().id,
-                    project:$auth.getCurrentProject().id
+                    project: $auth.getCurrentProject().id
                 };
                 console.log(saveChanel);
-                $restful.save({table: 'Rooms'}, saveChanel, function (resp){
-                   if(resp.success){
-                       $scope.conversionChanel.push(resp.data);
+                $restful.save({table: 'Rooms'}, saveChanel, function (resp) {
+                    if (resp.success) {
+                        $scope.conversionChanel.push(resp.data);
 
-                       $state.go('buzz.home.conversion', {conversionId: resp.data.id});
-                       $('#modal-create-group').modal('hide');
-                       $scope.newChanel = {};
-                   }
+                        $state.go('buzz.home.conversion', {conversionId: resp.data.id});
+                        $('#modal-create-group').modal('hide');
+                        $scope.newChanel = {};
+                    }
                 });
 
 
             };
 
-            var getJSON = function (file, callback) {
-                $http.get('scripts/data/' + file + '.json').success(function (resp) {
-                    callback(null, resp);
-                }).error(function (err) {
-                    callback(err, null);
-                })
+            $scope.getListRooms = function () {
+                var filter = [
+                    {
+                        property: 'project',
+                        type: 'text',
+                        comparison:'eq',
+                        value: $auth.getCurrentProject().id
+                    }
+                ];
+                $restful.get({table: "Rooms", filter: JSON.stringify(filter) }, function (resp){
+                    if(resp.success){
+                        $scope.conversionChanel = resp.data;
+                    }
+                });
             };
 
 
-            getJSON('chanels', function (err, resp){
-               if(err) throw err;
-                $scope.conversionChanel = resp;
-                $state.go('buzz.home.conversion', {conversionId: $scope.conversionChanel[0].id});
-            });
+            $scope.getListRooms();
+            /*var getJSON = function (file, callback) {
+             $http.get('scripts/data/' + file + '.json').success(function (resp) {
+             callback(null, resp);
+             }).error(function (err) {
+             callback(err, null);
+             })
+             };*/
+
+
+            /*getJSON('chanels', function (err, resp){
+             if(err) throw err;
+             $scope.conversionChanel = resp;
+             $state.go('buzz.home.conversion', {conversionId: $scope.conversionChanel[0].id});
+             });*/
 
 
             $scope.hehe = function (item) {
