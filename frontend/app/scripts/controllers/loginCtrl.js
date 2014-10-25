28100b03a5fff3a171f4fa936d5b94b98886ddac
thinhjs <thinhit@yahoo.com>
2014-10-25 23:15:54 +0700

    Finally function send message

--- frontend/app/scripts/controllers/loginCtrl.js
+++ frontend/app/scripts/controllers/loginCtrl.js
@@ -9,22 +9,16 @@ angular.module('Buzz')
                 $state.go('buzz.project');
             }
 
+            $scope.errMsg = "";
             $scope.register = function (item) {
-                console.log(item);
-                /*var registerInfo = {
-                 firstname: item.firstname,
-                 lastname: item.lastname,
-                 username: item.username,
-                 password: item.password,
-                 email: item.email
-                 };
-                 $http.post(appAPI + '/register', registerInfo)
-                 .success(function (resp) {
-                 console.log('register success');
-                 })
-                 .error(function (error) {
-                 console.log('register error');
-                 })*/
+                $auth.register(item, function (err, resp){
+                    if(resp.success){
+                        alert("Đăng ký thành công !");
+                        $scope.registerInfo = {}
+                    }else {
+                        alert("Tài khoản đã có người sử dụng");
+                    }
+                })
             };
 
 
