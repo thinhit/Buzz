798f0a659008525ea3eb51611891091b148de685
thinhjs <thinhit@yahoo.com>
2014-10-28 12:59:16 +0700

    Finally function send message

--- frontend/app/scripts/services/socket.io.js
+++ frontend/app/scripts/services/socket.io.js
@@ -1,5 +1,5 @@
 angular.module('Buzz').factory('$socket', function ($rootScope) {
-    var socket = window.io.connect('http://localhost:8889');
+    var socket = window.io.connect('http://thinh.me:8889');
     return {
         on: function (eventName, callback) {
             socket.on(eventName, function () {
